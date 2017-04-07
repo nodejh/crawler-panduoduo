5021b1b7248e125ab69ed7076b37a4bc9fc07357
nodejh <jianghangscu@gmail.com>
2017-04-07 20:16:42 +0800

    change start and end page number

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   startPage: 1,
-  endPage: 388683,
+  endPage: 389683,
   urlPrefix: 'http://www.panduoduo.net',
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
   mongodb: 'mongodb://localhost:27017/panduoduo-test',
