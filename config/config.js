107499560b1024406e2f96a8acef8e1a0c6ba37e
nodejh <jianghangscu@gmail.com>
2017-04-10 02:14:24 +0800

    使用async控制并发

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   endPage: 389683,
   urlPrefix: 'http://www.panduoduo.net',
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-  mongodb: 'mongodb://localhost:27017/panduoduo-test',
+  mongodb: 'mongodb://localhost:27017/panduoduo',
   delayMax: 20000000, // 最大间隔时间 (约 20000000 / 2000 ms)
   mysql: {
     host: 'localhost',
